7a712f6e589570bf053754d81135f29f4a4064f1
Sandro Simperl <sandrosimperl.cc@gmail.com>
2022-11-29 10:24:00 +0100

    updloaded files

--- com/cc/java/App.java
+++ com/cc/java/App.java
@@ -6,11 +6,16 @@ public class App {
      
     }
 
-    public static void output(String outputStr) {
-        System.out.println(outputStr);
+    public static void o( String o ) { System.out.println( o ); }
+    public static String c( String c ) {
+        switch ( c ) {
+            case "#r": return "\u001b[31m";
+            case "#g": return "\u001b[32m";
+            case "#y": return "\u001b[33m";
+            case "#b": return "\u001b[34m";
+            case "#p": return "\u001b[35m";
+            case "#c": return "\u001b[36m";
+            default: return "\u001b[0m";
+        }
     }
-
-
-
 }
-
